b5acef6ed12b888f66d15ddec58fd5254aadd590
Blabla <st.ten.veldhuis@gmail.com>
2016-05-05 22:30:32 +0200

    Update main.php

--- src/PocketRPG/main.php
+++ src/PocketRPG/main.php
@@ -6,6 +6,18 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\Server;
 
+use PocketRPG\Quests\commands\endquest;
+use PocketRPG\Quests\commands\listquest;
+use PocketRPG\Quests\commands\startquest;
+use PocketRPG\Quests\commands\seequest;
+
+use PocketRPG\RPG\commands\rpgstart;
+
+use PocketRPG\partysystem\commands\partyinvite;
+use PocketRPG\partysystem\commands\partycreate;
+use PocketRPG\partysystem\commands\partyleave;
+
+
 class main extends PluginBase {
   
   public function onLoad() {
