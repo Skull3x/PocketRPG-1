96d1f3380208eb81f13883ab9ce9fc6b6f377ede
Blabla <st.ten.veldhuis@gmail.com>
2016-05-05 11:34:04 +0200

    Update rpgstart.php

--- src/PocketRPG/RPG/commands/rpgstart.php
+++ src/PocketRPG/RPG/commands/rpgstart.php
@@ -63,7 +63,9 @@ class rpgstart extends PluginBase {
             } else {
               $p->sendMessage(TF:: AQUA . "You have joined the world as an assassin!");
               $bow = Item::get(Item::BOW, 0, 1);
+              $arrows = Item::get(Item::ARROW, 0, 128);
               $p->getInventory->addItem($bow);
+              $p->getInventory->addItem($arrows)
               $p->setPermission("class.chosen");
               $p->setPermission("class.archer");
               $p->switchLevel($level $cfglevel);
@@ -75,10 +77,23 @@ class rpgstart extends PluginBase {
             } elseif($p->hasPermission("class.special")) {
               $p->sendMessage(TF:: AQUA . "You have joined the world as an assassin!");
               $cloak = Item::get(Item::CLOCK, 0, 1);
-              $knive = Item::get(Item::FEATHER, 0, 1);
-              $p->getInventory->addItem($knive, $cloak);
+              $knife = Item::get(Item::FEATHER, 0, 1);
+              $p->getInventory->addItem($knife);
+              $p->getInventory->addItem($cloak);
               $p->setPermission("class.chosen");
               $p->setPermission("class.assassin");
+              $p->switchLevel($level $cfglevel);
+            }
+        
+        } elseif($args[0] == "thief") {
+            if($p->hasPermission("class.chosen")) {
+              $p->sendMessage(TF:: RED . "You have already picked a class!");
+            } elseif($p->hasPermission("class.special")) {
+              $p->sendMessage(TF:: AQUA . "You have joined the world as a Thief!");
+              $bone = Item::get(Item::BONE, 0, 1);
+              $p->getInventory->addItem($bone);
+              $p->setPermission("class.chosen");
+              $p->setPermission("class.thief");
               $p->switchLevel($level $cfglevel);
             }
         }
