4732e1f947b7888189ebcecb60d5dbd245c2f93b
Blabla <st.ten.veldhuis@gmail.com>
2016-05-05 11:38:36 +0200

    Update assassindagger.php

--- src/PocketRPG/RPG/resources/assassindagger.php
+++ src/PocketRPG/RPG/resources/assassindagger.php
@@ -22,28 +22,34 @@ use PocketRPG\main;
 
 class knightknive extends PluginBase implements Listener {
     
-    public function onFight(EntityDamageEvent $event) {  
-      if($event instanceof EntityDamageByEntityEvent && $event->getDamager() instanceof Player) {
+public function onFight(EntityDamageEvent $event) {  
+    if($event instanceof EntityDamageByEntityEvent && $event->getDamager() instanceof Player) {
         $hit = $event->getEntity();
         $damager = $event->getDamager();
-        $level = $this->config->get("RPG_LEVEL");
-        if($damager->getLevel() == $configlevel) {
-          if($damager->getItemInHand()->getId() == 388) {
-            $x = $hit->x;
-            $y = $hit->y;
-            $z = $hit->z;
-            $hitpos = $hit->getPosition(new Vector3($x, $y, $z));
-            $level->addParticle(new CriticalParticle($hitpos));
-            $this->setDamage(getDamage() + 3);
-          }
+        $cfglevel = $this->config->get("RPG_LEVEL");
+        if($damager->getLevel() == $cfglevel) {
+            if($p->hasPermission("class.mage")) {
+                if($damager->getItemInHand()->getId() == 388) {
+                $x = $hit->x;
+                $y = $hit->y;
+                $z = $hit->z;
+                $hitpos = $hit->getPosition(new Vector3($x, $y, $z));
+                $level->addParticle(new CriticalParticle($hitpos));
+                $this->setDamage(getDamage() + 3);
+                }
+            }
         }
-      }
-  }
+    }
+}
     public function onItemHeld(PlayerItemHeldEvent $event2) {
         $p = $event2->getPlayer();
-        if($p->getItemInHand()->getId() == 347) {
-            $effect = Effect::getEffect(14)->setDuration(1)->setAmplifier(1)->setVisible(true);
-            $p->addEffect($effect);
+        if($p->getLevel() == $cfglevel) {
+            if($p->getItemInHand()->getId() == 347) {
+                if($p->hasPermission("class.mage")) {
+                    $effect = Effect::getEffect(14)->setDuration(1)->setAmplifier(1)->setVisible(true);
+                    $p->addEffect($effect);
+                }
+            }
         }
     }
 }
