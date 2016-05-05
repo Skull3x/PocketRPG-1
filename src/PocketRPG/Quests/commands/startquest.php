575caafb60b2682c0355e7d7d44d6d505d175e1e
Blabla <st.ten.veldhuis@gmail.com>
2016-05-05 08:25:42 +0200

    fix.

--- src/PocketRPG/Quests/commands/startquest.php
+++ src/PocketRPG/Quests/commands/startquest.php
@@ -15,134 +15,121 @@ class startquest extends PluginBase {
   public function onCommand(CommandSender $p, Command $cmd, $label, array $args) {
     switch($cmd->getName()) {
       case "startquest":
-        switch(strtolower(array_shift($args))) {
-          case "1":
-            if($p->getExpLevel() > -1) {
+        if($args[0] == "1") {
+            if($p->hasPermission("quest.1.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > -1) {
               $p->sendMessage(TF::GRAY . "Quest started: Helping the Farmer");
               $p->sendMessage(TF::AQUA . "Collect 5 pieces of wheat.");
               $p->setPermission("quest.1.cancomplete");
-            } elseif($p->hasPermission("quest.1.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
               $p->sendMessage(TF::RED . "You are not the right level to start this quest.");
             }
-            break;
           
-          case "2":
-            if($p->getExpLevel() > 0) {
+        } elseif($args[0] == "2") {
+            if($p->hasPermission("quest.2.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > 0) {
               $p->sendMessage(TF::GRAY . "Quest started: Not enough Trees");
               $p->sendMessage(TF::AQUA . "Collect 16 Oak Saplings");
               $p->setPermission("quest.2.cancomplete");
-            } elseif($p->hasPermission("quest.2.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
             }
-            break;
             
-          case "3":
-            if($p->getExpLevel() > 1) {
+        } elseif($args[0] == "3") {
+            if($p->hasPermission("quest.3.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > 1) {
               $p->sendMessage(TF::GRAY . "Quest started: Searching the Stick");
               $p->sendMessage(TF::AQUA . "Collect 1 stick");
               $p->setPermission("quest.3.cancomplete");
-            } elseif($p->hasPermission("quest.3.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
             }
-            break;
-
-          case "4":
-            if($p->getExpLevel() > 2) {
+            
+        } elseif($args[0] == "4") {
+            if($p->hasPermission("quest.4.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > 2) {
               $p->sendMessage(TF::GRAY . "Quest started: Shearing the Sheap");
               $p->sendMessage(TF::AQUA . "Collect 1 shears");
               $p->setPermission("quest.4.cancomplete");
-            } elseif($p->hasPermission("quest.4.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
             }
-            break;
             
-          case "5":
-            if($p->getExpLevel() > 3) {
+        } elseif($args[0] == "5") {
+            if($p->hasPermission("quest.5.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > 3) {
               $p->sendMessage(TF::GRAY . "Quest started: The lost Gem");
               $p->sendMessage(TF::AQUA . "Collect 1 Emerald");
               $p->setPermission("quest.5.cancomplete");
-            } elseif($p->hasPermission("quest.5.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
             }
-            break;
             
-          case "6":
-            if($p->getExpLevel() > 4) {
+        } elseif($args[0] == "6") {
+            if($p->hasPermission("quest.6.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > 4) {
               $p->sendMessage(TF::GRAY . "Quest started: Freezingly Cold");
               $p->sendMessage(TF::AQUA . "Collect 32 ice blocks");
               $p->setPermission("quest.6.cancomplete");
-            } elseif($p->hasPermission("quest.6.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
             }
-            break;
             
-          case "7":
-            if($p->getExpLevel() > 5) {
+        } elseif($args[0] == "7") {
+            if($p->hasPermission("quest.7.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > 5) {
               $p->sendMessage(TF::GRAY . "Quest started: Gravedigger");
               $p->sendMessage(TF::AQUA . "Collect 5 bones and 5 rotten flesh");
               $p->setPermission("quest.7.cancomplete");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
-            } elseif($p->hasPermission("quest.7.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             }
-            break;
             
-          case "8":
-            if($p->getExpLevel() > 6) {
+        } elseif($args[0] == "8") {
+            if($p->hasPermission("quest.8.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > 6) {
               $p->sendMessage(TF::GRAY . "Quest started: Fish Galore");
               $p->sendMessage(TF::AQUA . "Collect 16 fishes");
               $p->setPermission("quest.8.cancomplete");
-            } elseif($p->hasPermission("quest.8.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
             }
-            break;
             
-          case "9":
-            if($p->getExpLevel() > 7) {
+        } elseif($args[0] == "9") {
+            if($p->hasPermission("quest.9.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > 7) {
               $p->sendMessage(TF::GRAY . "Quest started: Burning Souls");
               $p->sendMessage(TF::AQUA . "Collect 5 blaze powder and 5 blaze rods");
               $p->setPermission("quest.9.cancomplete");
-            } elseif($p->hasPermission("quest.9.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
             }
-            break;
             
-          case "10":
-            if($p->getExpLevel() > 8) {
+        } elseif($args[0] == "10") {
+            if($p->hasPermission("quest.10.cancomplete")) {
+              $p->sendMessage(TF:: RED . "You already started this quest!");
+            } elseif($p->getExpLevel() > 8) {
               $p->sendMessage(TF::GRAY . "Quest started: The Experiment");
               $p->sendMessage(TF::AQUA . "Collect 10 glowstone dust and 10 redstone");
               $p->setPermission("quest.10.cancomplete");
-            } elseif($p->hasPermission("quest.10.cancomplete")) {
-              $p->sendMessage(TF:: RED . "You already started this quest!");
             } else {
               $p->sendMessage(TF:: RED . "You are not the right level to start this quest.");
             }
-            break;
           
-          default:
+        } elseif(!isset($args[0])) {
             $p->sendMessage(TF:: RED . "Please specify the number of a quest.");
         }
-      return true;
-    default:
-      return false;
     }
+  return true;
   }
 }
 
