1c996c85dc836e88781c48d0465a4c73c821e83c
Blabla <st.ten.veldhuis@gmail.com>
2016-05-25 12:36:11 +0200

    Update Itemsunlosable.php

--- src/PocketRPG/itemsremain/Itemsunlosable.php
+++ src/PocketRPG/itemsremain/Itemsunlosable.php
@@ -3,8 +3,8 @@
 namespace PocketRPG/itemsremain;
 
 use pocketmine\PluginBase;
-use pocketmine\utils\TextFormat;
 use PocketRPG\main;
+use pocketmine\utils\Config;
 use pocketmine\event\Listener;
 use pocketmine\event\CraftItemEvent;
 use pocketmine\event\FurnaceBurnEvent;
@@ -15,4 +15,32 @@ use pocketmine\Server;
 
 class itemsunlosable extends PluginBase implements Listener {
   
+  public function onCraft(CraftItemEvent $event) {
+    $rpglvl = $this->config->get("RPG_LEVEL")
+    if($event->getPlayer()->getLevelByName() == "$rpglevel") {
+      $event->setCancelled();
+    }
+  }
+  
+  public function onBurn(FurnaceBurnEvent $event2) {
+    $rpglvl = $this->config->get("RPG_LEVEL")
+    if($event2->getPlayer()->getLevelByName() == "$rpglvl") {
+      $event2->setCancelled();
+    }
+  }
+  
+  public function onSmelt(FurnaceSmeltEvent $event3) {
+    $rpglvl = $this->config->get("RPG_LEVEL")
+    if($event3->getPlayer()->getLevelByName() == "$rpglevel") {
+      $event3->setCancelled();
+    }
+  }
+  
+  public function onDrop(PlayerDropEvent $event4) {
+    $rpglvl = $this->config->get("RPG_LEVEL")
+    if($event4->getPlayer()->getLevelByName() == "$rpglevel") {
+      $event4->setCancelled();
+    }
+  }
 }
+?>
