83d820d566aad324567b95e9d911ff1887c07193
Blabla <st.ten.veldhuis@gmail.com>
2016-05-25 10:19:02 +0200

    Create Itemsunlosable.php

---
+++ src/PocketRPG/itemsremain/Itemsunlosable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PocketRPG/itemsremain;
+
+use pocketmine\PluginBase;
+use pocketmine\utils\TextFormat;
+use PocketRPG\main;
+use pocketmine\event\Listener;
+use pocketmine\event\CraftItemEvent;
+use pocketmine\event\FurnaceBurnEvent;
+use pocketmine\event\FurnaceSmeltEvent;
+use pocketmine\event\PlayerDropItemEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class itemsunlosable extends PluginBase implements Listener {
+  
+}
