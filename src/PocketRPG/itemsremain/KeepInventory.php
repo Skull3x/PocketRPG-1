a9acd8f4213c592f76906d902ce2e263be87f784
Blabla <st.ten.veldhuis@gmail.com>
2016-05-26 13:47:01 +0200

    Create KeepInventory.php

---
+++ src/PocketRPG/itemsremain/KeepInventory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PocketRPG\itemsremain;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\PlayerDeathEvent;
+use pocketmine\utils\TextFormat;
+use PocketRPG\main;
+
+class KeepInventory extends PluginBase implements Listener {
+
+}
+?>
