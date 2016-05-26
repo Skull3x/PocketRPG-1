4c624b433b389e3b4d72e0fa3eb3171a8202d086
Blabla <st.ten.veldhuis@gmail.com>
2016-05-26 13:50:15 +0200

    Update KeepInventory.php

--- src/PocketRPG/itemsremain/KeepInventory.php
+++ src/PocketRPG/itemsremain/KeepInventory.php
@@ -5,6 +5,7 @@ namespace PocketRPG\itemsremain;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\Player;
+use pocketmine\utils\Config;
 use pocketmine\event\Listener;
 use pocketmine\event\PlayerDeathEvent;
 use pocketmine\utils\TextFormat;
@@ -12,5 +13,14 @@ use PocketRPG\main;
 
 class KeepInventory extends PluginBase implements Listener {
 
+  public function onDeath(PlayerDeathEvent $event) {
+    $p = $event->getPlayer();
+    $rpglvl = $this->config->get("RPG_LEVEL");
+    if($p->getLevelByName() == $rpglvl) {
+      $p->setKeepInventory();
+    } else {
+      $p->setKeepInventory(false);
+    }
+  }
 }
 ?>
